b98e02081c3dd1b5b206e9e8e37ed36fe2d01f06
Matus Chochlik <chochlik@gmail.com>
2014-10-19 17:31:57 +0200

    Added devel files

---
+++ devel/test23.cpp
@@ -0,0 +1,38 @@
+/**
+ *  .file devel/test21.cpp
+ *
+ *  Copyright 2012-2014 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#include <eagine/base/scope_exit.hpp>
+//------------------
+#include <iostream>
+#include <exception>
+
+void success(void)
+{
+	std::cout << "success" << std::endl;
+}
+
+void failure(void)
+{
+	std::cout << "failure" << std::endl;
+}
+
+int main(int argc, const char**)
+{
+	using namespace EAGine::base;
+
+	{
+		on_scope_exit se(&success, &failure);
+	}
+	try
+	{
+		on_scope_exit se(&success, &failure);
+		throw 1;
+	}
+	catch(...) { }
+
+	return 0;
+}
