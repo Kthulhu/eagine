e5718006317035d0baaa25365e3e267445848665
Matus Chochlik <chochlik@gmail.com>
2015-02-18 07:16:00 +0100

    Updated tribool and its test cases

--- include/eagine/base/tribool.hpp
+++ include/eagine/base/tribool.hpp
@@ -2,7 +2,7 @@
  *  @file eagine/base/tribool.hpp
  *  @brief Tri-state logic
  *
- *  Copyright 2012-2014 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2012-2015 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
@@ -14,19 +14,59 @@
 namespace eagine {
 namespace base {
 
+enum class _tribool_value_t : unsigned char
+{
+	_false = 0x00,
+	_true = 0x01,
+	_unknown = 0x02
+};
+
 struct indeterminate_t { };
 
 constexpr static const indeterminate_t indeterminate = {};
 
+class tribool;
+
+class weakbool
+{
+private:
+	typedef _tribool_value_t _value_t;
+	_value_t _value;
+
+	friend class tribool;
+
+	constexpr explicit
+	weakbool(_value_t value)
+	noexcept
+	 : _value(value)
+	{ }
+public:
+	constexpr
+	explicit 
+	operator bool (void) const
+	noexcept
+	{
+		return _value != _value_t::_false;
+	}
+
+	constexpr
+	bool operator ! (void) const
+	noexcept
+	{
+		return _value != _value_t::_true;
+	}
+
+	constexpr
+	bool is(indeterminate_t) const
+	{
+		return _value == _value_t::_unknown;
+	}
+};
+
 class tribool
 {
 private:
-	enum class _value_t : unsigned char
-	{
-		_false = 0x00,
-		_true = 0x01,
-		_unknown = 0x02
-	};
+	typedef _tribool_value_t _value_t;
 	_value_t _value;
 public:
 	constexpr
@@ -63,15 +103,24 @@ public:
 	}
 
 	constexpr
-	bool operator ~ (void) const
+	bool operator * (void) const
+	noexcept
 	{
 		return _value == _value_t::_unknown;
 	}
 
 	constexpr
-	bool is(indeterminate_t) const
+	weakbool operator ~ (void) const
+	noexcept
 	{
-		return _value == _value_t::_unknown;
+		return weakbool(_value);
+	}
+
+	constexpr
+	bool is(indeterminate_t) const
+	noexcept
+	{
+		return *(*this);
 	}
 
 	friend
@@ -79,7 +128,7 @@ public:
 	tribool operator == (tribool a, tribool b)
 	noexcept
 	{
-		return ((~a) || (~b))?
+		return (*a || *b)?
 			tribool{indeterminate}:
 			tribool{a._value == b._value};
 	}
@@ -89,7 +138,7 @@ public:
 	tribool operator != (tribool a, tribool b)
 	noexcept
 	{
-		return ((~a) || (~b))?
+		return (a.is(indeterminate) || b.is(indeterminate))?
 			tribool{indeterminate}:
 			tribool{a._value != b._value};
 	}
--- test/eagine/base/tribool.cpp
+++ test/eagine/base/tribool.cpp
@@ -4,7 +4,7 @@
  *
  *  .author Matus Chochlik
  *
- *  Copyright 2012-2014 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2012-2015 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
@@ -46,13 +46,31 @@ BOOST_AUTO_TEST_CASE(base_tribool_ops)
 	eagine::base::tribool b = false;
 	eagine::base::tribool c = eagine::base::indeterminate;
 
+	BOOST_ASSERT(bool(a));
+	BOOST_ASSERT(!!a);
+	BOOST_ASSERT(!*a);
+	BOOST_ASSERT(~a);
+	BOOST_ASSERT(!!~a);
 	BOOST_ASSERT(a == a);
-	BOOST_ASSERT(a == true);
+	BOOST_ASSERT(true == a);
+	BOOST_ASSERT(!bool(b));
+	BOOST_ASSERT(!b);
+	BOOST_ASSERT(!*b);
+	BOOST_ASSERT(!bool(~b));
+	BOOST_ASSERT(!~b);
 	BOOST_ASSERT(b == b);
 	BOOST_ASSERT(b == false);
+	BOOST_ASSERT(false == b);
 	BOOST_ASSERT(!(a == b));
+	BOOST_ASSERT(!(b == a));
+	BOOST_ASSERT(!bool(c));
+	BOOST_ASSERT(!!c);
+	BOOST_ASSERT(*c);
+	BOOST_ASSERT(~c);
+	BOOST_ASSERT(!~c);
 
 	BOOST_ASSERT(a != false);
+	BOOST_ASSERT(false != a);
 	BOOST_ASSERT(b != true);
 	BOOST_ASSERT(!(b != false));
 	BOOST_ASSERT(!(a != a));
@@ -72,14 +90,20 @@ BOOST_AUTO_TEST_CASE(base_tribool_ops)
 	BOOST_ASSERT(~(c == a));
 	BOOST_ASSERT(~(c == b));
 
+	BOOST_ASSERT(~(a != c));
+	BOOST_ASSERT(~(b != c));
+	BOOST_ASSERT(~(c != c));
+	BOOST_ASSERT(~(c != a));
+	BOOST_ASSERT(~(c != b));
+
 	BOOST_ASSERT(a && a);
 	BOOST_ASSERT(!(a && b));
 	BOOST_ASSERT(!(b && a));
 	BOOST_ASSERT(!(b && b));
-	BOOST_ASSERT(~(a && c));
+	BOOST_ASSERT(*(a && c));
 	BOOST_ASSERT(!(b && c));
-	BOOST_ASSERT(~(c && c));
-	BOOST_ASSERT(~(c && a));
+	BOOST_ASSERT(*(c && c));
+	BOOST_ASSERT(*(c && a));
 	BOOST_ASSERT(!(c && b));
 
 	BOOST_ASSERT(a || a);
@@ -88,9 +112,9 @@ BOOST_AUTO_TEST_CASE(base_tribool_ops)
 	BOOST_ASSERT(b || a);
 	BOOST_ASSERT(c || a);
 	BOOST_ASSERT(!(b || b));
-	BOOST_ASSERT(~(c || b));
-	BOOST_ASSERT(~(b || c));
-	BOOST_ASSERT(~(c || c));
+	BOOST_ASSERT(*(c || b));
+	BOOST_ASSERT(*(b || c));
+	BOOST_ASSERT(*(c || c));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
