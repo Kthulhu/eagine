275231a1e8ae57321b496bd12aecc2723659adee
Matus Chochlik <chochlik@gmail.com>
2015-05-16 08:44:07 +0200

    Minor changes in the build system config

--- config/CPP.cmake
+++ config/CPP.cmake
@@ -1,15 +1,12 @@
-#  Copyright 2012-2014 Matus Chochlik. Distributed under the Boost
+#  Copyright 2012-2015 Matus Chochlik. Distributed under the Boost
 #  Software License, Version 1.0. (See accompanying file
 #  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
 
-# we need C++11
-if(${CMAKE_COMPILER_IS_GNUCXX})
-	set(OGLPLUS_CPP11_COMPILER_SWITCH -std=c++1y)
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-	set(OGLPLUS_CPP11_COMPILER_SWITCH -std=c++1y)
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	add_definitions(-std=c++1y)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+	add_definitions(-std=c++1y)
 endif()
 # TODO add support for other compilers
-
-add_definitions(${OGLPLUS_CPP11_COMPILER_SWITCH})
 
